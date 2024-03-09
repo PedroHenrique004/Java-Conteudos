7e64f53ba88098be86d3094186ed020c604967e2
Pedro Santos <pedrohenriqueph004@gmail.com>
2024-03-09 11:09:41 -0300

    Passando as notas de forma dinâmica

--- src/Exercícios/Mod_8/MediaNotas.java
+++ src/Exercícios/Mod_8/MediaNotas.java
@@ -1,14 +1,26 @@
 package Exercícios.Mod_8;
+import java.util.Scanner;
 
 public class MediaNotas {
 
     public static void main(String[] args) {
-        double notaAluno1 = 10;
-        double notaAluno2 = 8;
-        double notaAluno3 = 6;
-        double notaAluno4 = 4;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a nota do aluno 1: ");
+        double notaAluno1 = scanner.nextDouble();
+
+        System.out.print("Digite a nota do aluno 2: ");
+        double notaAluno2 = scanner.nextDouble();
+
+        System.out.print("Digite a nota do aluno 3: ");
+        double notaAluno3 = scanner.nextDouble();
+
+        System.out.print("Digite a nota do aluno 4: ");
+        double notaAluno4 = scanner.nextDouble();
+
+        scanner.close();
 
         double media = (notaAluno1 + notaAluno2 + notaAluno3 + notaAluno4) / 4;
-        System.out.println(media);
+        System.out.println("A média das notas é: " + media);
     }
 }
